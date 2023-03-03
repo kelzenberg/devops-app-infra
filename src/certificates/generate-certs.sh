1825659a870e9f7d4042016e6f43b4c5e12a1344
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2023-03-03 16:19:25 +0100

    cleanup generating certificates script

--- src/certificates/generate-certs.sh
+++ src/certificates/generate-certs.sh
@@ -4,4 +4,5 @@ echo "Generating certificates..."
 MINIKUBE_IP=$(minikube ip)
 echo "Minikube IP is $MINIKUBE_IP"
 CERT_PATH="$(pwd)/src/certificates"
-mkcert -key-file ${CERT_PATH}/key.pem -cert-file ${CERT_PATH}/cert.pem localhost 127.0.0.1 ::1 $MINIKUBE_IP *.$MINIKUBE_IP
+rm -rf $CERT_PATH/key.pem $CERT_PATH/cert.pem
+mkcert -key-file $CERT_PATH/key.pem -cert-file $CERT_PATH/cert.pem localhost 127.0.0.1 ::1 $MINIKUBE_IP *.$MINIKUBE_IP nip.io *.nip.io *.$MINIKUBE_IP.nip.io
